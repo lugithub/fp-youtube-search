8eb48d7a9e9bf2b65d172c39b52033a4036231b5
lubin <lu.bin.lu.bin@gmail.com>
2017-11-23 23:20:33 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Either from 'data.either';
+
+import { Empty, searchVideos } from './youtube/model';
+import VideoSearch from './youtube/VideoSearch';
+import styles from './styles';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: Empty
+    };
+  }
+
+  searchVideos(term) {
+    searchVideos(term).fork((e) => this.showError(e), this.updateResults.bind(this));
+  }
+
+  showError(err) {
+    console.error(err);
+  }
+
+  updateResults(results) {
+    this.setState({ results });
+  }
+
+  render() {
+    return (
+      <VideoSearch onSearch={this.searchVideos.bind(this)} results={this.state.results}/>
+    );
+  }
+}
+
+export default App;
---
+++ src/youtube/model.js
@@ -0,0 +1,55 @@
+import { compose, composeP, invoker, lift, map, prop } from 'ramda';
+import Either from 'data.either';
+import Maybe from 'data.maybe';
+import Task from 'data.task';
+import daggy from 'daggy';
+
+const Video = daggy.tagged('Video', ['id', 'thumbnail', 'url', 'title']);
+
+export const Empty = Either.Left('Search for YouTube videos');
+
+// Alias for Url
+// type Url = String;
+
+// makeUrl :: String -> Url
+
+const makeUrl = (term) =>
+  `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=24&type=video&q=${term}&key=AIzaSyDoBHAuHjjKeANT_T5WV8XIjrYpAo3BiOU`; // xxxx is the Google API key
+
+// maybeMakeUrl :: String -> Maybe Url
+const maybeMakeUrl = (term) =>
+  term && term.length > 0
+    ? Maybe.Just(makeUrl(term))
+    : Maybe.Nothing()
+  ;
+
+// getJson -> HttpResponse -> Promise JSON
+const getJson = invoker(0, 'json');
+
+// httpGet :: String -> Task Error JSON
+const httpGet = (url) =>
+  new Task((rej, res) =>
+    fetch(url).then(composeP(res, getJson)).catch(rej)
+  );
+
+// maybeHttpGet :: e -> Maybe String -> Task Error (Either e JSON)
+const maybeHttpGet = (e) => (url) =>
+  url.cata({
+    Just: compose(lift(Either.Right), httpGet),
+    Nothing: () => Task.of(e)
+  });
+
+// toVideo :: JSON -> Video
+const toVideo = json => {
+  const {
+    id: { videoId },
+    snippet: { thumbnails: { high: { url: thumbnail } }, title }
+  } = json;
+  return Video(videoId, thumbnail, `https://www.youtube.com/watch?v=${videoId}`, title);
+};
+
+// toVideos :: JSON -> [Video]
+const toVideos = compose(map(toVideo), prop('items'));
+
+// searchVideos :: String -> Task Error (Either Empty [Video])
+export const searchVideos = compose(lift(lift(toVideos)), maybeHttpGet(Empty), maybeMakeUrl);
